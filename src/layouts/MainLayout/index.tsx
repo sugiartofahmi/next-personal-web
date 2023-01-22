2556725d2bdd3510e1904d5de9f776705f9d5ae2
sugiartofahmi <sugiartofahmi@gmail.com>
2023-01-22 14:25:32 +0700

    Fahmi Sugiarto | Update portfolio

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -6,7 +6,7 @@ type MainLayoutType = {
 
 const MainLayout: FC<MainLayoutType> = ({ children }) => {
   return (
-    <main className=" h-full w-full bg-gradient-to-r from-blue-500 to-green-400 flex flex-col md:flex-row px-5 py-5 gap-x-5 gap-y-5">
+    <main className="font-poppins h-full w-full bg-gradient-to-r from-blue-500 to-green-400 flex flex-col md:flex-row px-5 py-5 gap-x-5 gap-y-5">
       {children}
     </main>
   );

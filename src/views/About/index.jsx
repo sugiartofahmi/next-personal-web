28625d272844bbf29b8f8bfe4e325443fc3c022f
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-06 13:55:33 +0700

    improvement: update cv

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -23,24 +23,41 @@ const About = () => {
             <h1 className="md:text-4xl text-2xl font-extrabold text-gradient md:w-[210px] w-[150px]">
               Who am I ?
             </h1>
-            <p className="md:text-lg text-base md:pl-2 pl-1">
-              I am a student pursuing a degree in technology. I have experience
-              in web programming, especially in front-end development using
-              HTML, CSS and JavaScript. I also have skills in using frameworks
-              like React and Vue. I am very interested in doing an internship in
-              front-end web development and am confident that I can add positive
-              value to the company.
-            </p>
+            <h1 className="md:text-lg text-base md:pl-2 pl-1 font-normal leading-relaxed">
+              An informatics engineering student with experience in web
+              programming, especially in front end development using HTML, CSS
+              and JavaScript. Besides that, I also have expertise in using the
+              <a
+                href="https://react.dev/"
+                className="underline underline-offset-4 decoration-dotted decoration-1"
+              >
+                {" "}
+                React JS{" "}
+              </a>
+              framework and{" "}
+              <a
+                href="https://tailwindcss.com/"
+                className="underline underline-offset-4 decoration-dotted decoration-1"
+              >
+                {" "}
+                Tailwind CSS{" "}
+              </a>
+              . I am very interested in doing an internship in front-end web
+              development and I'm sure I can make a positive contribution for
+              the company
+            </h1>
           </section>
           <section className="flex flex-col gap-y-1">
             <h1 className="md:text-4xl text-2xl font-extrabold text-gradient md:w-[850px] ">
               What technology am I currently using ?
             </h1>
-            <ul className="md:text-lg text-base list-disc md:pl-7 pl-5">
-              <li>React JS</li>
-              <li>Javascript</li>
-              <li>Express JS</li>
-              <li>Postgresql</li>
+            <ul className="md:text-lg text-base list-disc pl-2 flex flex-wrap md:gap-3 gap-4">
+              <img className="w-[80px]" src="/js-icon.svg" alt="" />
+              <img className="w-[150px]" src="/node-icon.svg" alt="" />
+              <img className="w-[100px]" src="/react-icon.svg" alt="" />
+              <img className="w-[100px]" src="/tailwind-icon.svg" alt="" />
+              <img className="w-[90px]" src="/express-icon.svg" alt="" />
+              <img className="w-[100px]" src="/mongodb-icon.svg" alt="" />
             </ul>
           </section>
         </section>

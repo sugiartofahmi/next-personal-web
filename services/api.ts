4c7b9c2930de178fcce26d0dedf1a88ad3ad77b8
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-15 01:43:25 +0700

    improvement: update tech stack with next prisma

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ const config: AxiosRequestConfig = {
   headers: {
     "Content-Type": "application/json",
   },
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 };
 
 export const api = axios.create(config);

45cd9f8a42fee617af0e426dd021ea4d605f1d57
sugiartofahmi <sugiartofahmi@gmail.com>
2023-07-15 01:54:47 +0700

    improvement: update tech stack with next prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+declare global {
+  var prisma: PrismaClient;
+}
 
+import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
-prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
